7dc232f30cc082f40bb7df8ede261155c1c3dc16
Ayrton Souza <ayrtonsouza@icloud.com>
2022-01-15 00:16:26 -0300

    [:construction:] creating 'BaseEntity'

--- src/main/java/com/ayrtonsouza/codeflix/domain/entity/Category.java
+++ src/main/java/com/ayrtonsouza/codeflix/domain/entity/Category.java
@@ -2,25 +2,25 @@ package com.ayrtonsouza.codeflix.domain.entity;
 
 import java.util.UUID;
 
-public class Category {
-  private UUID id;
+public class Category extends BaseEntity {
+
   private String name;
 
   public Category() {
   }
 
   public Category(String name) {
+    super.generateUUID();
+    this.setName(name);
+  }
+
+  public Category(UUID id) {
+    super.setId(id);
   }
 
   public Category(UUID id, String name) {
-  }
-
-  public UUID getId() {
-    return this.id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
+    super.setId(id);
+    this.setName(name);
   }
 
   public String getName() {
