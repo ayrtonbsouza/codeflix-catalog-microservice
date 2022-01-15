3174bbbed4ebbd7d7c80831118ad74642dc21b7f
Ayrton Souza <ayrtonsouza@icloud.com>
2022-01-14 23:45:22 -0300

    [:tada:] initial commit

---
+++ src/main/java/com/ayrtonsouza/codeflix/CodeflixApplication.java
@@ -0,0 +1,13 @@
+package com.ayrtonsouza.codeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayrtonsouza/codeflix/CodeflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayrtonsouza.codeflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
